49b1acb09254517d27ce093daa5e2bd5e789a9dd
aaronsabellek <aaronsabellek@gmx.de>
2024-02-19 14:12:14 +0100

    testimonials content

--- capstone/src/Testimonial.js
+++ capstone/src/Testimonial.js
@@ -3,9 +3,10 @@
 function Testimonial(props) {
     return (
         <div class="testimonial_frame">
-            <div class="testimonial_image"></div>
+                <img  class="testimonial_portrait" src={props.image} alt="Portrait" />
             <div class="testimonial_box">
-                <p>{ props.name }</p>
+                <p class="testimonial_name">{ props.name }</p>
+                <p class="testimonial_rating">{ props.rating }/5&#9733;</p>
                 <p>{ props.text }</p>
             </div>
         </div>
--- capstone/src/Testimonials.js
+++ capstone/src/Testimonials.js
@@ -1,22 +1,34 @@
 import Testimonial from './Testimonial.js';
+import portrait_1 from './images/Portrait_1.jpg';
+import portrait_2 from './images/Portrait_2.jpg';
+import portrait_3 from './images/Portrait_3.jpg';
+import portrait_4 from './images/Portrait_4.jpg';
 
 function Testimonials(props) {
     const data = [
         {
+            image: portrait_1,
             name: "Hanna",
-            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.",
+            rating: "4.5",
+            text: "'Amazing food, amazing service, amazing restaurant!'",
         },
         {
+            image: portrait_2,
             name: "Justus",
-            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.",
+            rating: "5",
+            text: "'Great food at reasonable prices. Great service and the owners are terrific.'",
         },
         {
+            image: portrait_3,
             name: "Julia",
-            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.",
+            rating: "4",
+            text: "'Do yourself a favor and treat yourself here, you won't be sorry.'",
         },
         {
+            image: portrait_4,
             name: "Max",
-            text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor.",
+            rating: "5",
+            text: "'Best bruchetta in town. You always feel welcome and the ambiance is relaxing.'",
         },
     ];
 
@@ -26,7 +38,9 @@ function Testimonials(props) {
                 <div class="dishes margin">
                     {data.map((testimonial) => (
                         <Testimonial
+                            image={testimonial.image}
                             name={testimonial.name}
+                            rating={testimonial.rating}
                             text={testimonial.text}
                         />
                     ))}
