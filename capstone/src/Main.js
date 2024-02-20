9afeb8a5d5d1eff1f74bd789cfd7b53aca395813
aaronsabellek <aaronsabellek@gmx.de>
2024-02-20 16:12:18 +0100

    remove unwanted comments

--- capstone/src/Main.js
+++ capstone/src/Main.js
@@ -4,52 +4,13 @@ import { Navigate, useNavigate, Routes, Route } from 'react-router-dom';
 import { useState, useReducer } from "react";
 
 function Main(props) {
-    /*
-    const seededRandom = function (seed) {
-        var m = 2**35 - 31;
-        var a = 185852;
-        var s = seed % m;
-        return function () {
-            return (s = s * a % m) / m;
-        };
-    }
 
-    const fetchAPI = function(date) {
-        let result = [];
-        let random = seededRandom(date.getDate());
-
-        for(let i = 17; i <= 23; i++) {
-            if(random() < 0.5) {
-                result.push(i + ':00');
-            }
-            if(random() < 0.5) {
-                result.push(i + ':30');
-            }
-        }
-        return result;
-    };
-    const submitAPI = function(formData) {
-        return true;
-    };
-    */
-    const initialState = /*{availableTimes:  fetchAPI(new Date())}*/ {availableTimes: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]}
+    const initialState = {availableTimes: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]}
     function updateTimes() {
         return {availableTimes: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]}
     }
 
     const [availableTimes, dispatch] = useReducer(updateTimes, initialState);
-    /*
-    function updateTimes(state, date) {
-        return /*{availableTimes: fetchAPI(new Date(date))}
-    }
-
-    const navigate = useNavigate();
-    function submitForm (formData) {
-        if (submitAPI(formData)) {
-            navigate("/confirmed")
-        }
-    }
-    */
 
     return (
 
