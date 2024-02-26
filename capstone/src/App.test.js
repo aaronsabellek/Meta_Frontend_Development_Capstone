1457b8d65aac80eabe480a4fb23b68a11bfd09a0
aaronsabellek <aaronsabellek@gmx.de>
2024-02-26 13:39:36 +0100

    update App test

--- capstone/src/App.test.js
+++ capstone/src/App.test.js
@@ -1,10 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import BookingForm from './components/BookingForm.js';
+import App from './App';
 
-const availableTimes = {timeSlots: ["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"]};
+test('renders learn react link', () => {
 
-test('Renders the BookingForm label', () => {
-  render(<BookingForm availableTimes={availableTimes} />);
-  const labelElement = screen.getByText("Choose date");
-  expect(labelElement).toBeInTheDocument();
-})
+});
